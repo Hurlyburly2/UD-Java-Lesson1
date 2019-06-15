2e51a1af80a8b8d3e9bf22546ebb9f1a909df51a
Hurlyburly2 <dougdougmann@gmail.com>
2019-06-15 16:29:59 -0400

    finished first varialble lesson

--- src/Hello.java
+++ src/Hello.java
@@ -4,5 +4,15 @@ public class Hello {
 
         System.out.println("Hello, World!");
 
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        System.out.println(myTotal);
+
+        int myDifference = 1000 - myTotal;
+        System.out.println(myDifference);
     }
 }
